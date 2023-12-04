563d694e07247ec79b7032cb4d4a4ebddd67f36e
Acmsdy <2895369781@qq.com>
2023-12-04 10:45:11 +0800

    配置跨域资源共享

---
+++ test1/src/main/java/com/example/demo/common/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.common.config;
+
+/**
+ * @ClassName：CorsConfig
+ * @Author：Acmsdy
+ * @Date：2023-12-04 10:06
+ * @Describe：
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
